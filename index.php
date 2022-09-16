a74c25fbaacf5eda13bf8ffd81b1d8ec11a21ba7
patrickmcintire <patrick.mcintire@gmail.com>
2022-09-16 02:30:56 -0400

    add logic for date parsing, conversion to sql datetime and record update

--- index.php
+++ index.php
@@ -38,6 +38,16 @@ $recordsRemainder = findTheRecords($conn, $orderCommentsProcessed, null, 0, true
 echo '<h2>All Other Records (' . count($recordsRemainder) . ')</h2>';
 presentTheRecords($recordsRemainder);
 
+// Reset Counter
+$orderCommentsProcessed = [];
+
+// Gather Records that include an Expected Ship Date
+$recordsExpected = findTheRecords($conn, $orderCommentsProcessed, 'Expected Ship Date');
+echo '<h2>Updating Orders with Expected Ship Date (' . count($recordsExpected) . ')</h2>';
+
+// Update the Shipdates
+updateExpectedShipDate($conn, $recordsExpected);
+
 function findTheRecords($conn, &$orderCommentsProcessed, string $textString = null, int $mode = 0, bool $getRemainder = false): array
 {
     $records = [];
@@ -78,6 +88,38 @@ function presentTheRecords($records): void
     }
     echo '</tbody>';
     echo '</table>';
+}
+
+function updateExpectedShipDate($conn, $records) : void
+{
+    foreach ($records as $record) {
+
+        $extractedDate = [];
+
+        // Parse Shipdate
+        preg_match("^\\d{1,2}/\\d{2}/\\d{2}^", $record['comments'], $extractedDate);
+
+        if (count($extractedDate)) {
+            
+            // Get datetime format for sql
+            $sqlDate = date("Y-m-d H:i:s", strtotime(reset($extractedDate)));
+
+            // Status update for logging
+            echo 'updating ' . $record['orderid'] . ' with ship date ' . $sqlDate . ' ... <br>';
+
+            // Update record            
+            $query = $conn->prepare("UPDATE
+                sweetwater_test
+                SET shipdate_expected = '" . $sqlDate . "'
+                WHERE orderid = '" . $record['orderid'] . "'");
+
+            try {
+                $query->execute();
+            } catch (Exception $e) {
+                die('Query failed: ' . $e->getMessage());
+            }
+        }
+    }
 }
 
 exit;
